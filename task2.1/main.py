11adae8929eaf5f457d4f54282cec3b0ac4a65bb
Eldar Tursunbaev <eldar.tursunbaev@wolterskluwer.com>
2022-07-01 22:57:12 +0600

    Task 2.1 added

---
+++ task2.1/main.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+somestr = "Eldar Tursunbaev"
+def findLen(somestr):
+    counter = 0   
+    for i in somestr:
+        counter += 1
+    return counter
+ 
+ 
+print(findLen(somestr))
